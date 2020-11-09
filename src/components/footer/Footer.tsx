f6a690f6c7e19e23c0bc487046939ca6a76f0fe6
Carla de Beer <carla.debeer.uk@gmail.com>
2020-11-09 18:49:01 +0100

    Fix css responsive footer issue

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Badge from 'react-bootstrap/Badge';
 
 import './Footer.scss';
 
+import Badge from 'react-bootstrap/Badge';
 import linkedIn from '../../assets/icons/Linkedin_1.svg';
 import gitHub from '../../assets/icons/Github_1.svg';
 import twitter from '../../assets/icons/Twitter_1.svg';
@@ -10,8 +10,26 @@ import twitter from '../../assets/icons/Twitter_1.svg';
 export default class Footer extends React.Component {
     setNavHeader = () => {
       if (window.innerWidth.valueOf() < 1024) {
-        return (<br />);
+        return (
+          <div>
+            <Badge className="badge badge-pill badge-secondary">Machine Learning</Badge>
+            <br />
+            <Badge className="badge badge-pill badge-secondary">Software Engineering</Badge>
+            <br />
+            <Badge className="badge badge-pill badge-secondary">Backend Development</Badge>
+            <br />
+            <Badge className="badge badge-pill badge-secondary">Fullstack Development</Badge>
+          </div>
+        );
       }
+      return (
+        <div>
+          <Badge className="badge badge-pill badge-secondary">Machine Learning</Badge>
+          <Badge className="badge badge-pill badge-secondary ml-2">Software Engineering</Badge>
+          <Badge className="badge badge-pill badge-secondary ml-2">Backend Development</Badge>
+          <Badge className="badge badge-pill badge-secondary ml-2">Fullstack Development</Badge>
+        </div>
+      );
     };
 
     render() {
@@ -34,18 +52,17 @@ export default class Footer extends React.Component {
               >
                 <img src={linkedIn} width="25px" height="25px" alt="Icon" />
               </a>
-              <a className="ml-2" href="https://github.com/Carla-de-Beer" target="_blank" rel="noopener noreferrer">
+              <a
+                className="ml-2"
+                href="https://github.com/Carla-de-Beer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src={gitHub} width="25px" height="25px" alt="Icon" />
               </a>
             </div>
             <div className="text-fields" style={{ marginTop: '90px' }}>
-              <Badge className="badge badge-pill badge-secondary">Machine Learning</Badge>
               {this.setNavHeader()}
-              <Badge className="badge badge-pill badge-secondary ml-2">Software Engineering</Badge>
-              {this.setNavHeader()}
-              <Badge className="badge badge-pill badge-secondary ml-2">Backend Development</Badge>
-              {this.setNavHeader()}
-              <Badge className="badge badge-pill badge-secondary ml-2">Fullstack Development</Badge>
             </div>
             <div>
               <p className="footer-style pb-5 pt-3 footer-text-small mb-lg-2" style={{ marginTop: '50px' }}>
