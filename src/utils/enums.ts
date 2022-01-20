0bb4ad4a95b7ffd92f0f4b414b80bd504c4aac36
Carla de Beer <carla.debeer.uk@gmail.com>
2022-01-20 23:27:00 +0100

    Disable lint rules for enums.ts

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,6 +1,10 @@
+// eslint-disable-next-line no-shadow,no-unused-vars
 enum PageSize {
+    // eslint-disable-next-line no-unused-vars
     SMALL = 576,
+    // eslint-disable-next-line no-unused-vars
     MEDIUM = 824,
+    // eslint-disable-next-line no-unused-vars
     LARGE = 1025,
 }
 
