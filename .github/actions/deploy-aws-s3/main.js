6189b6bf804d319df4482f5c2247ee27b33a8612
Carla de Beer <carla.debeer.uk@gmail.com>
2023-09-23 21:02:15 +0200

    Replace composite actions with reusable workflow
    (cherry picked from commit 36131cf2e545e38aaac38f2cddd1a0e51a1f390f)

--- .github/actions/deploy-aws-s3/main.js
+++ .github/actions/deploy-aws-s3/main.js
@@ -4,11 +4,12 @@ const exec = require('@actions/exec');
 const main = () => {
     const bucket = core.getInput('bucket', {required: true});
     const bucketRegion = core.getInput('bucket-region', {required: true});
-    const distFolder = core.getInput('dist-folder', {required: true});
+    const artifactFolder = core.getInput('artifact-folder', {required: true});
 
     const bucketUri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync ${distFolder} ${bucketUri} --region ${bucketRegion}`)
-        .then(r => core.notice('Deployment done'));
+    exec.exec(`aws s3 sync ${artifactFolder} ${bucketUri} --region ${bucketRegion}`)
+        .then(() => core.notice('Deployment to S3 completed'))
+        .catch((error) => core.error(`Deployment was unsuccessful due ro error ${error}`));
 }
 
 main();
